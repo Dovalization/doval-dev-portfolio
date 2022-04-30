2f0ffe51604c97c22aeccb2af91e2d8e1fcc74c6
Guilherme Doval <guilherme.doval@aquiris.com.br>
2022-04-30 17:41:48 -0300

    ✨Feat: Add About Section

--- src/layout/Home/AboutSection/index.tsx
+++ src/layout/Home/AboutSection/index.tsx
@@ -1,5 +1,11 @@
 import * as S from "./styles";
 
 export const AboutSection = () => {
-  return <S.Wrapper>Sobre mim</S.Wrapper>;
+  return (
+    <S.Wrapper>
+      <h1>
+        Quem é <span>Guilherme Doval?</span>
+      </h1>
+    </S.Wrapper>
+  );
 };
--- src/layout/Home/AboutSection/styles.ts
+++ src/layout/Home/AboutSection/styles.ts
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const Wrapper = styled.section`
   background-color: ${({ theme }) => theme.colors.black};
   min-height: 100vh;
+  padding: 15rem 0;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
 `;
