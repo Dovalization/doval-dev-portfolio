f1ccec0215637f68df32fbcf0bf1c700b7bb7e9b
Guilherme Doval <doval.guilherme94@gmail.com>
2022-04-30 11:33:21 -0300

    ✨Feat: Render Header/Footer Globally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import SEOConfig from "@/../next-seo.config";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
 import { GlobalStyle } from "@styles/global";
 import theme from "@styles/theme";
 import { DefaultSeo } from "next-seo";
@@ -7,19 +9,23 @@ import { ThemeProvider } from "styled-components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <DefaultSeo
-        {...SEOConfig}
-        additionalLinkTags={[
-          {
-            rel: "icon",
-            href: "/images/favicon.ico",
-          },
-        ]}
-      />
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <DefaultSeo
+          {...SEOConfig}
+          additionalLinkTags={[
+            {
+              rel: "icon",
+              href: "/images/favicon.ico",
+            },
+          ]}
+        />
+        <GlobalStyle />
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
+    </>
   );
 }
 
