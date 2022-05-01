5342c9b47cfebf7a16e9ec54ec3a8bd9e6709b20
Guilherme Doval <doval.guilherme94@gmail.com>
2022-05-01 17:20:36 -0300

    ✨Feat: Link  behavior  to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,25 @@
 import { StyledIcon } from "@styled-icons/styled-icon";
+import Link from "next/link";
 import { ButtonHTMLAttributes } from "react";
 import * as S from "./styles";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon?: StyledIcon;
+  href?: string;
 }
 
-export const Button = ({ children, onClick, icon: Icon }: ButtonProps) => {
+export const Button = ({
+  children,
+  onClick,
+  icon: Icon,
+  href,
+}: ButtonProps) => {
   return (
-    <S.Wrapper onClick={onClick}>
-      {children}
-      {Icon && <Icon size={20} />}
-    </S.Wrapper>
+    <Link href={href || "#"} passHref>
+      <S.Wrapper onClick={onClick} as={href ? "a" : "button"}>
+        {children}
+        {Icon && <Icon size={20} />}
+      </S.Wrapper>
+    </Link>
   );
 };
