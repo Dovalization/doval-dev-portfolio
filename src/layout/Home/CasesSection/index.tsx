cea149d0ecc9b482ae03bf18f247442065f47bbd
Guilherme Doval <doval.guilherme94@gmail.com>
2022-05-13 10:26:23 -0300

    ✨Feat: Add Mock Cases

--- src/layout/Home/CasesSection/index.tsx
+++ src/layout/Home/CasesSection/index.tsx
@@ -6,25 +6,13 @@ interface CasesSectionProps {
   projects: Project[];
 }
 
-const mockProject: Project = {
-  banner: "/images/project-banner.jpg",
-  logo: "/images/project-logo.png",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  id: "1",
-  links: {
-    code: "github.com/",
-    live: "https://www.google.com/",
-  },
-  stack: ["React", "NextJS", "TypeScript"],
-  title: "Maria Luiza - Centro de Estética",
-};
-
-export const CasesSection = ({}: CasesSectionProps) => {
+export const CasesSection = ({ projects }: CasesSectionProps) => {
   return (
     <S.Wrapper>
       <S.ProjectsContainer>
-        <Collapsible project={mockProject} />
+        {projects.map((project, index) => (
+          <Collapsible project={project} key={index} />
+        ))}
       </S.ProjectsContainer>
     </S.Wrapper>
   );
