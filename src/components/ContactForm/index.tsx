d9666c45e87c8aca1b03dd1b8dfb4a3154817ec0
Guilherme Doval <doval.guilherme94@gmail.com>
2022-06-01 22:50:57 -0300

    ✨Feat: Add Contact Form

---
+++ src/components/ContactForm/index.tsx
@@ -0,0 +1,125 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { CheckCircle, Loader, Send } from "styled-icons/feather/";
+import * as yup from "yup";
+import { Button } from "../Button";
+import { Input } from "../Input";
+import * as S from "./styles";
+
+interface SubcriptionFormFields {
+  email: string;
+  name: string;
+}
+
+const SubscriptionFormSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Deve ser um e-mail válido")
+    .required("O e-mail é obrigatório"),
+  name: yup
+    .string()
+    .min(2, "Deve ter no mínimo 2 caracteres")
+    .required("O nome é obrigatório"),
+  message: yup
+    .string()
+    .min(10, "Deve ter no mínimo 10 caracteres")
+    .required("A mensagem é obrigatória"),
+});
+
+export const ContactForm = () => {
+  const { formState, handleSubmit, ...rest } = useForm<SubcriptionFormFields>({
+    resolver: yupResolver<yup.AnyObjectSchema>(SubscriptionFormSchema),
+  });
+
+  const [submitButtonState, setSubmitButtonState] = useState<
+    "default" | "loading" | "success"
+  >("default");
+
+  const { isSubmitSuccessful, isSubmitting } = formState;
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      setSubmitButtonState("success");
+    }
+
+    if (isSubmitting) {
+      setSubmitButtonState("loading");
+    }
+
+    if (!isSubmitting && !isSubmitSuccessful) {
+      setSubmitButtonState("default");
+    }
+  }, [isSubmitSuccessful, isSubmitting]);
+
+  const submitButtonIcons = {
+    default: Send,
+    loading: Loader,
+    success: CheckCircle,
+  };
+
+  const submitButtonText = {
+    default: "Enviar",
+    loading: "Enviando...",
+    success: "Enviado!",
+  };
+
+  const submitForm = useCallback(async (data: SubcriptionFormFields) => {
+    // try {
+    //   const res = await axios.post("/api/contact", data);
+    //   console.log(res.data);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    console.log(data);
+  }, []);
+
+  return (
+    <FormProvider {...{ formState, handleSubmit, ...rest }}>
+      <S.Wrapper
+        onSubmit={handleSubmit(submitForm)}
+        $isSubmitSuccessful={isSubmitSuccessful}
+      >
+        <S.Title>
+          Entre em <span>Contato</span>
+        </S.Title>
+        <S.FieldsContainer>
+          <Input
+            name="name"
+            type="text"
+            placeholder="Nome"
+            disabled={isSubmitting || isSubmitSuccessful}
+          />
+          <Input
+            name="email"
+            type="email"
+            placeholder="E-mail"
+            disabled={isSubmitting || isSubmitSuccessful}
+          />
+          <Input
+            name="message"
+            type="textarea"
+            placeholder="Mensagem..."
+            disabled={isSubmitting || isSubmitSuccessful}
+          />
+        </S.FieldsContainer>
+        <S.SuccessMessage open={isSubmitSuccessful}>
+          Obrigado por seu contato! {"👋🏻"}
+          <span>
+            Podes também conhecer mais sobre mim em meu{" "}
+            <Link href="https://www.linkedin.com/in/guilherme-doval/">
+              LinkedIn
+            </Link>
+          </span>
+        </S.SuccessMessage>
+        <Button
+          type="submit"
+          icon={submitButtonIcons[submitButtonState]}
+          disabled={isSubmitting || isSubmitSuccessful}
+        >
+          {submitButtonText[submitButtonState]}
+        </Button>
+      </S.Wrapper>
+    </FormProvider>
+  );
+};
---
+++ src/components/ContactForm/styles.ts
@@ -0,0 +1,112 @@
+import { Wrapper as Button } from "@components/Button/styles";
+import styled, { keyframes } from "styled-components";
+
+interface ContactFormStyleProps {
+  $isSubmitSuccessful: boolean;
+}
+
+const fadeInUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(1rem);
+    
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+export const Title = styled.h2`
+  text-align: center;
+
+  span {
+    color: ${({ theme }) => theme.colors.accent.regular};
+  }
+`;
+
+export const FieldsContainer = styled.div`
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  gap: 1.5rem;
+  margin: 2.5rem 0 1.5rem;
+
+  *:disabled {
+    display: none;
+  }
+`;
+
+export const Wrapper = styled.form<ContactFormStyleProps>`
+  position: relative;
+  ${Button} {
+    position: relative;
+    margin: 0 auto;
+  }
+
+  ${Title} {
+    display: ${({ $isSubmitSuccessful }) =>
+      $isSubmitSuccessful ? "none" : "block"};
+  }
+
+  ${FieldsContainer} {
+    display: ${({ $isSubmitSuccessful }) =>
+      $isSubmitSuccessful ? "none" : "flex"};
+  }
+  overflow: hidden;
+  height: 100%;
+  background: ${({ theme }) => theme.colors.blue.medium};
+  border-radius: 1rem;
+  width: 100%;
+  padding: 5rem 2.5rem;
+  place-self: center;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    max-width: 70%;
+    padding: 4rem 6rem;
+  }
+`;
+
+export const SuccessMessage = styled.dialog`
+  &[open] {
+    /* Positioning */
+
+    position: relative;
+
+    /* Display & Box Model */
+    align-items: center;
+    margin-bottom: 2.5rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    width: 100%;
+    /* Color */
+    background: transparent;
+    border: none;
+    color: ${({ theme }) => theme.colors.white};
+    /* Text */
+    text-align: center;
+    font-size: ${({ theme }) => theme.fontSizes.medium};
+    font-weight: 600;
+
+    animation: ${fadeInUp} 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
+
+  span {
+    display: block;
+    line-height: 1.5;
+    font-size: ${({ theme }) => theme.fontSizes.small};
+    a {
+      color: ${({ theme }) => theme.colors.accent.dark};
+
+      transition: color 0.1s ease-in-out;
+      &:hover {
+        color: ${({ theme }) => theme.colors.accent.regular};
+      }
+    }
+  }
+`;
