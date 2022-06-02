9e0b45032d8fa3f0907c8cfe2e58fc88cbd4911f
Guilherme Doval <doval.guilherme94@gmail.com>
2022-06-01 22:49:10 -0300

    ✨Feat: Add Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,56 @@
+import { InputHTMLAttributes, useCallback, useState } from "react";
+import { useFormContext } from "react-hook-form";
+import * as S from "./styles";
+
+interface InputProps
+  extends InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
+  name: string;
+  type?: "textarea" | "text" | "email" | "password";
+}
+
+export const Input = ({ name, ...props }: InputProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const {
+    watch,
+    formState: { errors },
+    register,
+  } = useFormContext();
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!watch(name));
+  }, [name, watch]);
+
+  return (
+    <>
+      <S.Wrapper
+        $isErrored={!!errors[name]}
+        $isFilled={isFilled}
+        $isFocused={isFocused}
+      >
+        {props.type === "textarea" ? (
+          <textarea
+            {...props}
+            {...register(name)}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+          />
+        ) : (
+          <input
+            {...props}
+            {...register(name)}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+          />
+        )}
+      </S.Wrapper>
+      {errors[name] && <S.Error>{errors[name]?.message}</S.Error>}
+    </>
+  );
+};
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,74 @@
+import styled, { css } from "styled-components";
+
+interface InputStyleProps {
+  $isFocused: boolean;
+  $isFilled: boolean;
+  $isErrored: boolean;
+}
+
+export const Wrapper = styled.div<InputStyleProps>`
+  position: relative;
+  width: 100%;
+  padding: 1rem;
+  border-radius: 0.25rem;
+
+  outline: 3px solid ${({ theme }) => theme.colors.blue.dark};
+  background: ${({ theme }) => theme.colors.blue.dark};
+  font-size: 1rem;
+
+  ${({ $isErrored }) =>
+    $isErrored &&
+    css`
+      border-color: lightcoral;
+    `}
+
+  ${({ $isFocused }) =>
+    $isFocused &&
+    css`
+      outline-color: ${({ theme }) => theme.colors.accent.regular};
+    `}
+
+  ${({ $isFilled }) =>
+    $isFilled &&
+    css`
+      outline-color: ${({ theme }) => theme.colors.accent.regular};
+    `}
+
+    > input, textarea {
+    background: inherit;
+    width: 100%;
+    font-family: inherit;
+    font-weight: medium;
+    border: none;
+    outline: none;
+    font-weight: 600;
+    color: ${({ theme }) => theme.colors.white};
+
+    ::placeholder {
+      color: ${({ theme }) => theme.colors.blue.light};
+      font-weight: 500;
+      background: inherit;
+    }
+  }
+
+  > textarea {
+    min-height: 8rem;
+    resize: vertical;
+
+    &::-webkit-resizer {
+      --light-blue: ${({ theme }) => theme.colors.blue.light};
+      border-style: solid;
+      border-width: 2px;
+      border-color: transparent var(--light-blue) var(--light-blue) transparent;
+    }
+  }
+
+  transition: outline-color 0.1s;
+`;
+
+export const Error = styled.span`
+  color: #ffd91a;
+  font-weight: 500;
+  text-align: right;
+  width: 100%;
+`;
